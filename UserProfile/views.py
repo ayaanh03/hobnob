41b84498004cbf1b555099896651fa27d9239cfc
Rashmi sahu <sahurashmi321@gmail.com>
2020-08-26 22:51:30 +0530

    Created user profile API.

--- UserProfile/views.py
+++ UserProfile/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from UserProfile.models import *
+from UserProfile.serializer import *
+
+
+class ProfileView(APIView):
+    def post(self, request):
+
+        if request.data.get("your_role") is None:
+            breakpoint()
+            serializer = Profile2Serializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response("Congratulation!!! your Profile data added")
+
+        elif request.data.get("your_role") == "SOCIAL MEDIA STAR":
+
+            serializer = SocialMediaSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(
+                    "Congratulation!!! your Profile data added for Social MEDIA Star Role"
+                )
+
+        elif request.data.get("your_role") == "MOM/DAD INFLUENCER":
+            serializer = MomInfluencerSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(
+                    "Congratulation!!! your Profile data added for Mom/Dad Influencer Role"
+                )
+
+        elif request.data.get("your_role") == "CAMPUS AMBASSADOR":
+            serializer = CampusAmbassadorSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(
+                    "Congratulation!!! your Profile data added for Campus Ambassador Role"
+                )
+
+        else:
+            serializer = WorkingProfessionalSerializer(data=request.data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(
+                    "Congratulation!!! your Profile data added for working Professional Role"
+                )
+
+
+class InterestView(APIView):
+    def post(self, request):
+        serializer = InterestsSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response("Congratulation!!! your field of Interests Added")
