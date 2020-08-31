b961e36dfa11af0d2a88bfdd119253aae65cfc0b
Ayaan Hussain <ayaanh@cmu.edu>
2020-08-31 18:11:18 +0530

    working api

--- UserProfile/views.py
+++ UserProfile/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from UserProfile.models import *
-from UserProfile.serializer import *
+from UserProfile.serializers import *
 
 
 class ProfileView(APIView):
