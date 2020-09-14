582a8f634e093955388c90370530063a4e553dde
Ayaan Hussain <ayaanh@cmu.edu>
2020-09-14 18:56:26 +0530

    /name is frontend fixed urls

--- HobNob/urls.py
+++ HobNob/urls.py
@@ -22,6 +22,7 @@ from django.views.generic.base import TemplateView  # new
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("", TemplateView.as_view(template_name="home.html"), name="home"),  # new
+    path("home/", TemplateView.as_view(template_name="home.html"), name="home"),  # new
+    path("name/", TemplateView.as_view(template_name="name.html"), name="name"),
     path("", include("UserProfile.urls")),
 ]
--- UserProfile/urls.py
+++ UserProfile/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 from UserProfile.views import *
 
 urlpatterns = [
-    path("profile/", get_profile, name="profile"),
-    # path("interest/", InterestView.as_view(), name="interest"),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("interest/", InterestView.as_view(), name="interest"),
 ]
